a3caef23a6bb4aed2ba9cf57033e948bc242a02b
Matheus de Assis <assis4002@gmail.com>
2020-10-27 19:54:30 -0300

    Entities implemented

---
+++ Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Exercise02_StringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get;  set; }
+        public Comment()
+        {
+
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Entities/Post.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Exercise02_StringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+    }
+}
